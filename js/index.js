c061d804fb70bbf52bb2ea9482915708a3feca16
sami-colon <abhishek.personal.email@gmail.com>
2019-12-12 17:14:58 +0530

    second amendments

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ function desetAll(){
 	document.getElementById('our-team-link').style.color = "white";
 	document.getElementById('stories-link').style.color = "white";
 	document.getElementById('thanks-link').style.color = "white";
+	document.getElementById('why-link').style.color = "white";
+	document.getElementById('contribute-link').style.color = "white";
 }
 document.getElementById('home-link').addEventListener('click', function(){
 	desetAll();
@@ -24,4 +26,12 @@ document.getElementById('stories-link').addEventListener('click', function(){
 document.getElementById('thanks-link').addEventListener('click', function(){
 	desetAll();
 	document.getElementById('thanks-link').style.color = "black";
+});
+document.getElementById('why-link').addEventListener('click', function(){
+	desetAll();
+	document.getElementById('why-link').style.color = "black";
+});
+document.getElementById('contribute-link').addEventListener('click', function(){
+	desetAll();
+	document.getElementById('contribute-link').style.color = "black";
 });
